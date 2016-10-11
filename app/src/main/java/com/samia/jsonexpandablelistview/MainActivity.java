1fb1fca29eadace01ced917b9624beffb7c363ea
samiashazlly <samia.shazlly@gmail.com>
2016-10-11 16:11:59 +0200

    commit

--- app/src/main/java/com/samia/jsonexpandablelistview/MainActivity.java
+++ app/src/main/java/com/samia/jsonexpandablelistview/MainActivity.java
@@ -1,8 +1,12 @@
 package com.samia.jsonexpandablelistview;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
@@ -12,8 +16,9 @@ import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.json.JSONObject;
 
@@ -21,40 +26,76 @@ import org.json.JSONObject;
 public class MainActivity extends Activity implements
         OnChildClickListener, OnGroupExpandListener,
         OnGroupCollapseListener {
+
+    private String urlString = "https://api.myjson.com/bins/3car6";
     private ExpandableListAdapter adapter = null;
+    String response = null ;
+    private JSONObject jsonObject;
+    private ExpandableListView elv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (isNetworkAvailable()) {
+            Toast.makeText(MainActivity.this, "Network Connected", Toast.LENGTH_LONG).show();
+        }
 
-        InputStream inputStream = getResources().openRawResource(R.raw.data);
-        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-        String str;
-        StringBuffer buf = new StringBuffer();
+        elv = (ExpandableListView) findViewById(R.id.elv);
+        fetchJSON();
 
-        try {
-            while ((str = in.readLine()) != null) {
-                buf.append(str);
-                buf.append('\n');
+    }
+
+    public boolean isNetworkAvailable() {
+        boolean connected ;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        connected = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
+        return connected;
+    }
+
+    public void fetchJSON(){
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    conn.connect();
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                    }
+                    String crappyPrefix = "null";
+
+                    if(response.startsWith(crappyPrefix)){
+                        response = response.substring(crappyPrefix.length(), response.length());
+                    }
+                    jsonObject = new JSONObject(response);
+                    handler.sendEmptyMessage(0);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
+        }
+        );
+        thread.start();
+    }
 
-            in.close();
-
-            JSONObject jsonObject = new JSONObject(buf.toString());
-
-            ExpandableListView elv = (ExpandableListView) findViewById(R.id.elv);
-
+    public Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
             adapter = new JSONExpandableListAdapter(getLayoutInflater(), jsonObject);
             elv.setAdapter(adapter);
-
-            elv.setOnChildClickListener(this);
-            elv.setOnGroupExpandListener(this);
-            elv.setOnGroupCollapseListener(this);
-        } catch (Exception e) {
-            Log.e(getClass().getName(), "Exception reading JSON", e);
+            elv.setOnChildClickListener(MainActivity.this);
+            elv.setOnGroupExpandListener(MainActivity.this);
+            elv.setOnGroupCollapseListener(MainActivity.this);
         }
-    }
+    };
 
     @Override
     public boolean onChildClick(ExpandableListView parent, View v,
